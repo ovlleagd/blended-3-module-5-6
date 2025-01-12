7bfae5f928b608f43611a6cc385082c1b7cdab0e
Oleksiy Kost <AlekseyKo2208@gmail.com>
2025-01-12 11:29:34 +0200

    task3-5

--- js/task-05.js
+++ js/task-05.js
@@ -5,3 +5,12 @@ const numbers = [10, 25, 48, 7, 32, 18];
 //Завдання: Знайдіть найбільше число за допомогою reduce.
 //Очікуваний результат:
 //48
+
+const result = numbers.reduce((acc, element) => {
+  if (element > acc) {
+    acc = element;
+  }
+  return acc;
+}, 0);
+
+console.log(result);
