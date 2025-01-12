413783c651097cd33beac91c55b11da9f1a6f5e0
Oleksiy Kost <AlekseyKo2208@gmail.com>
2025-01-12 12:55:44 +0200

    task10-11

--- js/task-11.js
+++ js/task-11.js
@@ -1,9 +1,11 @@
 //Сортування рядків за довжиною
 //Дано той самий масив рядків:
 
-const words = ['banana', 'apple', 'grape', 'orange', 'kiwi'];
+const words = ["banana", "apple", "grape", "orange", "kiwi"];
 //Завдання: Відсортуйте рядки за їхньою довжиною (від найкоротшого до найдовшого).
 //Очікуваний результат:
 
 //['kiwi', 'apple', 'grape', 'banana', 'orange']
 
+const result = words.toSorted((a, b) => a.localeCompare(b));
+console.log(result);
